120f27444c5f1bee3e1e8ccf7d7dd9689139a2f8
Harrison Milburn <harrison.milburn@tharsus.co.uk>
2026-05-21 00:37:18 +0100

    works better with a higher spring rate, converges around 20Nmm i.e. stiff spring = not much dimensional change therefore not much change with stiffer spring (convergence)

--- simulations/force feedback/main.py
+++ simulations/force feedback/main.py
@@ -7,7 +7,7 @@ rear_knee = Joint(id="rear knee")
 foot = Joint(id="foot")
 
 front_thigh = Link(front_servo, front_knee, thigh_length_mm, angle=Angle.from_degrees(135), rate=None, id="front thigh")
-rear_thigh = Link(rear_servo, rear_knee, thigh_length_mm, angle=Angle.from_degrees(46), rate=None, id="rear thigh")
+rear_thigh = Link(rear_servo, rear_knee, thigh_length_mm, angle=Angle.from_degrees(45), rate=None, id="rear thigh")
 front_calf = Link(front_knee, foot, calf_length_mm, id="front calf")
 rear_calf = Link(rear_knee, foot, calf_length_mm, id="rear calf")
 
